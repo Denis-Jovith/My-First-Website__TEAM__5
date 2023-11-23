f435d10aa8f367745ec80fd5761af0f5951ffc03
Denis-Jovith <140693355+Denis-Jovith@users.noreply.github.com>
2023-11-23 21:50:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Team 5</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Team 5</h1>
+        <p>Explore our collaborative projects and get to know our team.</p>
+    </header>
+
+    <section class="container">
+        <div class="box">
+            <h2>Our Projects</h2>
+            <p>Discover the amazing projects created by our talented team members.</p>
+        </div>
+
+        <div class="box">
+            <h2>Meet the Team</h2>
+            <p>Learn more about each team member and their skills and contributions.</p>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Team 5</p>
+    </footer>
+</body>
+</html>
